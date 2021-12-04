dbc5526edc1efd078a836a94789c5dd0960153b8
unknown <awsdev747@gmail.com>
2021-12-04 15:26:12 +0530

    java demos

---
+++ day 1/DeclarationVarible.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class DeclarationVarible {
+
+	public static void main(String arg[]) {
+
+		int number1 = 20;
+		int number2 = 10;
+		int result;
+
+		result = number1 + number2; // add logic
+
+		System.out.print(" sum of two numbers  " + result);
+
+	}
+
+}
---
+++ day 1/DemoFirstprg.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class DemoFirstprg {
+
+	public static void main(String arg[]) {
+
+	
+		System.out.print(" hi how are you ");
+
+	}
+
+}
